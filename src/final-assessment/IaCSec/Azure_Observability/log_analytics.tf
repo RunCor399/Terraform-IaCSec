040a5b65c812e263fc57d7c70ef8e2df371a8c4f
Manuel Enrique Colotti s297014 <s297014@studenti.polito.it>
2023-09-01 15:35:08 +0200

    removed final assessment

--- src/final-assessment/IaCSec/Azure_Observability/log_analytics.tf
+++
@@ -1,9 +0,0 @@
-
-resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
-    name = var.log-analytics-workspace.name
-    location = var.log-analytics-workspace.location
-    resource_group_name = var.observability_rg.name
-    sku = "PerGB2018"
-
-    depends_on = [azurerm_resource_group.observability_rg]
-}
